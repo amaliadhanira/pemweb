8e3f4072dcd7a9b1fb21f43196e91dbba100de68
aldaputri99 <aldaputri99@gmail.com>
2020-05-01 14:09:45 +0700

    tambahan js

--- klinik4002/assets/js/dt_jumlah_antrean.js
+++ klinik4002/assets/js/dt_jumlah_antrean.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
 
-	/* JQUERY/AJAX DATATABLES DOKTER */
+	/* JQUERY/AJAX DATATABLES JUMLAH ANTREAN */
 
 	var table;
 
-	//DATATABLES DOKTER
-	table = $('#table_dokter').DataTable({
+	//DATATABLES JUMLAH ANTREAN
+	table = $('#table_jumlah_antrean').DataTable({
     	"processing": true,
     	"serverSide": true,
     	"order": [],
     	"language": {
-      	"emptyTable": "Anda belum memiliki antrean"
+      	"emptyTable": "Belum ada antrean hari ini"
     	},
 
     	"ajax": {
-      		"url": "http://localhost/klinik4002/klinik/data_dokter",
+      		"url": "http://localhost/klinik4002/klinik/data_jumlah_antrean",
       		"type": "POST",
       		"dataType": "JSON"
     	},
 
     	"columnDefs": [{
-      		"targets": [0, -1],
+      		"targets": [0],
       		"orderable": false,
     	},],
   	});
---
+++ klinik4002/assets/js/dt_laporan_pemeriksaan.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+
+  /* JQUERY/AJAX DATATABLES LAPORAN PEMERIKSAAN */
+
+  var aksi;
+  var table;
+  var no_antrean;
+
+  //DATATABLES LAPORAN PEMERIKSAAN
+  table = $('#table_laporan_pemeriksaan').DataTable({
+    "processing": true,
+    "serverSide": true,
+    "order": [],
+    "language": {
+      "emptyTable": "Anda belum memiliki antrean"
+    },
+
+    "ajax": {
+      "url": "http://localhost/klinik4002/klinik/data_laporan_pemeriksaan",
+      "type": "POST",
+      "dataType": "JSON"
+    },
+
+    "columnDefs": [{
+      "targets": [0, -1],
+      "orderable": false,
+    },],
+  });
+
+  function reload_table(){
+    table.ajax.reload(null, false);
+  }
+
+  //ON CLICK BUTTON RINCIAN LAPORAN
+  $('tbody').on('click', '#rincian_laporan', function(){
+
+    id_laporan = $(this).data("id_laporan");
+    aksi = 'rincian';
+
+    $.ajax({
+      url: "http://localhost/klinik4002/klinik/this_resep_obat/" + id_laporan,
+      type: "GET",
+      dataType: "JSON",
+      success: function(data){
+        $('#obat').empty();
+        $('#nama_dokter').text(data[0].nama_dokter);
+        $('#spesialis').text(data[0].nama_spesialisasi);
+        $('#tgl_periksa').text(data[0].tgl_periksa);
+        $('#diagnosa').text(data[0].diagnosa);
+        $.each(data, function(i, item){
+          $('#obat').append('<li>' + data[i].nama_obat + '</li>');
+        });
+        $('#modal_rincian_laporan').modal('show');
+      },
+      error: function (jqXHR, textStatus, errorThrown){
+        alert('Terjadi error dalam pengambilan data');
+      }
+    });
+  });
+
+  /*
+  //SET ID DOKTER WHEN DOKTER OPTION SELECTED
+  $('#dokter').on('change', function(){
+    $id_dokter = $("#dokter option:selected").val();
+    $('[name="id_dokter"]').val($id_dokter);
+  }).trigger("change");
+
+  //ON CLICK BUTTON SIMPAN (MODAL)
+  $('#btn_simpan').on('click', function(){
+
+    $('#btn_simpan').text('Menyimpan...');
+    $('#btn_simpan').prop('disabled', true);
+
+    var url;
+
+    if (aksi == 'add') {
+      url = "http://localhost/klinik4002/klinik/daftar_antrean";
+    } else {
+      url = "http://localhost/klinik4002/klinik/ubah_antrean";
+    }
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: $('#form_antrean').serialize(),
+      dataType: "JSON",
+      success: function(data){
+        console.log(data);
+
+        if (data.success){
+          $('#modal_antrean').modal('hide');
+          $id_dokter = $("#dokter option:eq(0)").val();
+          $('[name="id_dokter"]').val($id_dokter);
+          reload_table();
+        }
+
+        $('#btn_simpan').text('Simpan');
+        $('#btn_simpan').prop('disabled', false);
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+
+        alert('Terjadi error dalam pendaftaran atau pengubahan tanggal antrean');
+
+        $('#btn_simpan').text('Simpan');
+        $('#btn_simpan').prop('disabled', false);
+      }
+    });
+  });
+
+  //ON CLICK BUTTON BATALKAN
+  $('tbody').on('click', '#batalkan_antrean', function(){
+    no_antrean = $(this).data("no_antrean");
+
+    if (confirm('Apakah Anda yakin ingin membatalkan antrean?')){
+      $.ajax({
+        url: "http://localhost/klinik4002/klinik/batalkan_antrean/" + no_antrean,
+        type: "POST",
+        data: $('#form_antrean').serialize(),
+        dataType: "JSON",
+        success: function(data){
+          $('#modal_antrean').modal('hide');
+          reload_table();
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+          alert('Terjadi error dalam pembatalan antrean');
+        }
+      });
+    }
+  });
+  */
+
+  /* END OF JQUERY/AJAX DATATABLES LAPORAN PEMERIKSAAN */
+
+})
